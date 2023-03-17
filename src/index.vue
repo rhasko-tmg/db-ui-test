29612de9319cbd6ab0e338e0e5303284b2a51a26
Ruslan Hasko <rhasko@techmatesgroup.com>
2023-03-17 10:55:43 +0200

    update

---
+++ src/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="index">
+    This is the test Vitebook website
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'index',
+};
+</script>
+
+<style lang="scss">
+.index {
+
+}
+</style>
